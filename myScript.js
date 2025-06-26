90462e209486e463c0a609306fddfb5c0360a5a1
daneilcarlo <160308997+daneilcarlo@users.noreply.github.com>
2025-06-26 09:09:42 +0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,4 @@
+document.write('<a href="https://facebook.com/">facebook</a><br>');
+document.write('<a href="D:/OneDrive/Documents/CPE06l-Website/logoblack.png">picture</a><br>');
+document.write('<a href="delicacies.html">delicacies</a><br>');
+
